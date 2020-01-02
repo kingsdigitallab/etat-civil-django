bbe8fb8c14d1c7b636025a5d0568e9bab7cb916b
Miguel Vieira <jmvieira@gmail.com>
2020-01-02 10:13:00 +0000

    Add local copy of geonames place app.

---
+++ etat_civil/geonames_place/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import (ClassDescription, Country, FeatureClass,
+                     Place)
+
+
+@admin.register(ClassDescription)
+class ClassDescriptionAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'code']
+
+
+@admin.register(FeatureClass)
+class FeatureClassAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['class_description', 'country', 'feature_class']
+    list_display = ['address', 'class_description', 'country']
+    list_filter = ['class_description', 'country']
+    search_fields = ['address', 'country__name', 'country__code']
---
+++ etat_civil/geonames_place/apps.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8
+from django.apps import AppConfig
+
+
+class GeonamesPlaceConfig(AppConfig):
+    name = 'etat_civil.geonames_place'
