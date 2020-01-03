9f18fafd68e66dad62e8e785bb14d6a6b54f2a14
Miguel Vieira <jmvieira@gmail.com>
2020-01-03 18:59:22 +0000

    🐛 Fix: Typos in title.

--- etat_civil/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ etat_civil/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -14,7 +14,7 @@ def update_site_forward(apps, schema_editor):
         id=settings.SITE_ID,
         defaults={
             "domain": "etatcivil.ac.uk",
-            "name": "Etat Civil",
+            "name": "État Civil",
         },
     )
 
