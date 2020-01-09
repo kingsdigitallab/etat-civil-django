69bd2bd3980b3133605e87ae5ff582aed7d94a42
Miguel Vieira <jmvieira@gmail.com>
2020-01-09 21:07:45 +0000

    👌 Improve: Views to download locations and flows as CSV files.

--- etat_civil/deeds/urls.py
+++ etat_civil/deeds/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
-from etat_civil.deeds.views import geojson_view
+from etat_civil.deeds.views import (
+    geojson_view,
+    flowmap_flows_view,
+    flowmap_locations_view,
+)
 
 app_name = "deeds"
 
-urlpatterns = [path("geojson/", view=geojson_view, name="geojson")]
+urlpatterns = [
+    path("flowmap/flows/", view=flowmap_flows_view, name="flowmap_flows"),
+    path("flowmap/locations/", view=flowmap_locations_view, name="flowmap_locations"),
+    path("geojson/", view=geojson_view, name="geojson"),
+]
--- etat_civil/deeds/views.py
+++ etat_civil/deeds/views.py
@@ -1,6 +1,8 @@
-from django.http import JsonResponse
+import csv
+from django.http import HttpResponse, JsonResponse
 from django.views import View
 from etat_civil.deeds.models import Person
+from etat_civil.geonames_place.models import Place
 
 
 class GeoJSONView(View):
@@ -11,3 +13,37 @@ class GeoJSONView(View):
 
 
 geojson_view = GeoJSONView.as_view()
+
+
+class FlowmapLocationsView(View):
+    http_method_names = ["get"]
+
+    def get(self, request, *args, **kwargs):
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = 'attachment; filename="locations.csv"'
+
+        csv_writer = csv.writer(response, quoting=csv.QUOTE_NONNUMERIC)
+        csv_writer.writerow(["id", "name", "lat", "lon"])
+        csv_writer.writerows(Place.places_to_list())
+
+        return response
+
+
+flowmap_locations_view = FlowmapLocationsView.as_view()
+
+
+class FlowmapFlowsView(View):
+    http_method_names = ["get"]
+
+    def get(self, request, *args, **kwargs):
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = 'attachment; filename="flows.csv"'
+
+        csv_writer = csv.writer(response, quoting=csv.QUOTE_NONNUMERIC)
+        csv_writer.writerow(["origin", "dest", "count"])
+        csv_writer.writerows(Person.persons_to_flows())
+
+        return response
+
+
+flowmap_flows_view = FlowmapFlowsView.as_view()
