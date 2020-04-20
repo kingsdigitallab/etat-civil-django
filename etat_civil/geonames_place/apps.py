43b641a900df427aaed50dee2b9ca8ce5fbf1337
Miguel Vieira <jmvieira@gmail.com>
2020-04-20 11:26:27 +0100

    👌 Improve: Add app name; add geonames id as a search field.

--- etat_civil/geonames_place/apps.py
+++ etat_civil/geonames_place/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 
 class GeonamesPlaceConfig(AppConfig):
     name = "etat_civil.geonames_place"
+    verbose_name = "Geonames"
+    verbose_name_plural = "Geonames"
